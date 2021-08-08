d97a9de92be318b1eb8085deee2b8cdf7552b7ac
syahroel712 <syahroel712@gmail.com>
2021-08-08 17:02:41 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	const phi = 3.14
+	fmt.Println(phi)
+
+	const ( 
+		alpha = 8
+		name = "Syahrul"
+	)
+
+	fmt.Println(alpha)
+	fmt.Println(name)
+
+}
+
+
+
+
+
+
+// catatan
+
+// constant ialah variable yg nilainya tidak bisa diubah lagi jika sudah di beri nilai
+// cara pembuatan mirip dengan variable dan kata kuncinya const
+// saat buat const wajib isi nilainya
+
+// const juga bisa di deklarasikan secara banyak
+	// const ( 
+	// 	phi = 3.14
+	// 	name = "Syahrul"
+	// )
