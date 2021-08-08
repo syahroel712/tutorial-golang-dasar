7bdf7983bbda11ef34730a3bbc48111d4c518779
syahroel712 <syahroel712@gmail.com>
2021-08-08 15:51:44 +0700

    Tipe Data Boolean

---
+++ tipeDataBoolean.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main (){
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+}
+
+
+
+
+// catatan
+// tipe data boolean yg nilainya true / false
+// di go kata kuncinya bool
+
