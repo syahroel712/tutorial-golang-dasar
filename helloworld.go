a5394d3d3444149885c9bdd423af86cc6415644b
syahroel712 <syahroel712@gmail.com>
2021-08-08 15:34:02 +0700

    Hello World

---
+++ helloworld.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello world")
+}
+
+
+
+// catatan
+// =======
+// untk membuat program golang kita hasru menjalankan di dalam func main
+// kita harus menggunakan package main
+// untuk mencetak tulisan kita bisa import dari package fmt
+// di go boleh pakai ;
+
+// untuk membuild/mengkompile ketikan perintah dibawah dan akan menghasilkan file binary tanpa ekstensi
+	// go build nama_file.go
+
+// unutk menjalankan hasil file binary
+	// ./nama_file
+
+// untuk menjalankan tanpa membuild
+	// go run nama_file.go
+
+
+
