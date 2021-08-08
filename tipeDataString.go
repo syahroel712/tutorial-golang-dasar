b4ba212696ebf64ffdf77f7ac66b8325b32ec63d
syahroel712 <syahroel712@gmail.com>
2021-08-08 15:59:02 +0700

    Tipe Data String

---
+++ tipeDataString.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Syahrul")
+	fmt.Println("Syahrul 712")
+	fmt.Println(len("Syahrul 712"))
+	fmt.Println("Syahrul 712"[0])
+}
+
+
+
+
+// catatan
+// =================
+// string adalah tipe data kumpulan karakter
+// jumlah karakter string bisa nol sampai tidak terhingga
+// String kata kuncinya di go string
+// Nilai data string di go selalu diawali dengan karakter ""
+// ada banyak function yg bisa digunakan di string
+	// contoh
+	// len("string") => menghitung jumlah string
+	// "string"[number] => mengambil karakter pada posisi yg ditentukan dan hasilkan bukan karakter tapi adalah binary dari string tersebut
