f1e0fd38f4d8f92d010afb71fdc73007d39d08d3
syahroel712 <syahroel712@gmail.com>
2021-08-09 21:05:13 +0700

    Function

---
+++ function.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+	sayHello()
+}
+
+func sayHello(){
+	fmt.Println("Hi world")
+}
+
+
+// catatan
+
+// sebelumnya kita sudah mengeanl sebuah function yg wajib dibuat agar program kita bisa berjalan, yaitu function main
+// function adalah sebuah blok kode yg sengaja dibuat agar bisa digunakan berulang2
+// cara buat function adalah dengan kata kunci func lalu di ikuti nama_function dan blok kode isi functionnya
+	// func iniFunction(){
+		// blok kode
+	// }
+
+// setelah function dibuat, maka cara memanggilnya dengan menggunakan kata kunci nama_function dan diikuti kurung buka dan kurung tutup
+	// iniFunction()
+
+	
