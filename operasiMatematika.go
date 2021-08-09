52b2a3f0b36913aea8ebaaaaae6a207e5997b5dd
syahroel712 <syahroel712@gmail.com>
2021-08-09 08:58:21 +0700

    Operasi matematika

---
+++ operasiMatematika.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a = 10
+	var b = 5
+	var c = 2
+
+	var hasil = a * b / c 
+	fmt.Println(hasil)
+
+
+	var result = 10 + 10
+	fmt.Println(result)
+
+
+	var i = 10
+	i += 10;
+	fmt.Println(i)
+
+	
+	i++;
+	fmt.Println(i)
+
+}
+
+
+// catatan 
+
+// operatornya mirip dengan bahasa pemograman lainnya => * / + - %
+
+// augmented assigment mirip bahasa lain, melakukan operasi dengan nilainya sendiri
+	// a += 10
+	// b -= 2
+	// b *= 2
+	// b /= 2
+	// b %= 2
+
+// unary operator, mempersingkat orerasi matematika
+	// a++
+	// a--
