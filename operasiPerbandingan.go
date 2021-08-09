f55657a8424c085c4e7c97e1340a3fb3e2a43677
syahroel712 <syahroel712@gmail.com>
2021-08-09 09:05:15 +0700

    Operasi Perbandingan

---
+++ operasiPerbandingan.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var name1 = "Syahrul"
+	var name2 = "Syahrul"
+
+	var hasil = name1 == name2
+	fmt.Println(hasil)
+
+
+	var angka1 = 100
+	var angka2 = 200
+
+	fmt.Println(angka1 > angka2)
+	fmt.Println(angka1 < angka2)
+	fmt.Println(angka1 == angka2)
+	fmt.Println(angka1 != angka2)
+
+}
+
+
+
+
+
+
+// catatan
+
+// operasi perbandingan , orasi utk membadingkan 2 buah data
+// hasilnya selalu boolean, jika benar true, jika salah false
+// operator perbandingan
+	// >
+	// <
+	// >=
+	// <=
+	// ==
+	// !=
