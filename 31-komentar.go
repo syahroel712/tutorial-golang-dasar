e30d4f63b6dbaac9e7e86ec9dcbf98d16d8b3b8f
syahroel712 <syahroel712@gmail.com>
2021-08-11 10:02:47 +0700

    Komentar

---
+++ 31-komentar.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// ini komentar
+
+
+	/**
+	ini komentar
+	*/
+
+}
+
+
+
+
+// catatan 
+// komentar terbaik pada kode alah kode itu sendiri
+// saat buat kode, kita perlu buat kode yg mudah unutk dibaca
+// namun kadang kita jug butuh komentar di kode tsb
+// jenis komentar
+	// singgle line -> //
+	// multiline -> /**    */
