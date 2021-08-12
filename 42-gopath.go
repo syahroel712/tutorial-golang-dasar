86ceab76479a721cd7822f787aaac30a8390bc9d
syahroel712 <syahroel712@gmail.com>
2021-08-12 08:56:22 +0700

    GOPATH

---
+++ 42-gopath.go
@@ -0,0 +1,9 @@
+
+
+
+
+// catatan gopath
+// GOPATH adalah environtment variable yg berisikan lokasi tempat kita menyimpan project dan library go lang
+// gopath wajib di buat ketika kita mulai membuat aplikasi lebih dari satu file atau butuh menggunkan library dari luar
+// cara settingnya di windows buka edit environment, kemudian tambahkan variable baru degann nam GOPATH lalu setting lokasi simpan filenya
+// /setting gopath akan berpengaruh nantinya untuk aplikasi yg akan kita buat nantinya
